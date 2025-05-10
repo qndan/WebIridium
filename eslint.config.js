e17863f313c70a613457792a0cf0343cae8ddfe3
qndan <80284796+qndan@users.noreply.github.com>
2025-05-09 19:19:31 -0700

    add react eslint

--- eslint.config.js
+++ eslint.config.js
@@ -2,6 +2,7 @@ import js from "@eslint/js";
 import globals from "globals";
 import reactHooks from "eslint-plugin-react-hooks";
 import reactRefresh from "eslint-plugin-react-refresh";
+import react from "eslint-plugin-react";
 import tseslint from "typescript-eslint";
 
 export default tseslint.config(
@@ -16,13 +17,29 @@ export default tseslint.config(
     plugins: {
       "react-hooks": reactHooks,
       "react-refresh": reactRefresh,
+      react,
     },
     rules: {
       ...reactHooks.configs.recommended.rules,
+      ...react.configs["jsx-runtime"].rules,
       "react-refresh/only-export-components": [
         "warn",
         { allowConstantExport: true },
       ],
+
+      // custom stuff
+      "react/function-component-definition": [
+        "error",
+        {
+          "namedComponents": "arrow-function",
+          "unnamedComponents": "arrow-function",
+        },
+      ],
+    },
+    settings: {
+      "react": {
+        "version": "detect",
+      },
     },
   },
 );
