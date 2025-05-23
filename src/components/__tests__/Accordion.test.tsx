70e2f1f4531c34bf9d95978df5bea1f4ad024da0
qndan <80284796+qndan@users.noreply.github.com>
2025-05-22 18:48:18 -0700

    adjust accordion tests

--- src/components/__tests__/Accordion.test.tsx
+++ src/components/__tests__/Accordion.test.tsx
@@ -1,64 +1,20 @@
 import { describe, it, expect } from "vitest";
-import { render, screen, fireEvent } from "@testing-library/react";
-import { userEvent } from "@testing-library/user-event";
-import Accordion from "../accordion/Accordion";
-import AccordionItem from "../accordion/AccordionItem";
+import { render, screen } from "@testing-library/react";
+import PropertyAccordion from "../property-accordion/PropertyAccordion";
+import PropertyAccordionItem from "../property-accordion/PropertyAccordionItem";
 
 describe("Accordion", () => {
   it("should render its children", () => {
     render(
-      <Accordion>
-        <AccordionItem title="hello">test</AccordionItem>
-        <AccordionItem title="hello2">test2</AccordionItem>
-      </Accordion>,
+      <PropertyAccordion defaultValue={["test", "test2"]}>
+        <PropertyAccordionItem title="hello" value="test">test</PropertyAccordionItem>
+        <PropertyAccordionItem title="hello2" value="test2">test2</PropertyAccordionItem>
+      </PropertyAccordion>,
     );
 
     expect(screen.getByText("hello")).toBeInTheDocument();
     expect(screen.getByText("hello2")).toBeInTheDocument();
     expect(screen.getByText("test")).toBeInTheDocument();
     expect(screen.getByText("test2")).toBeInTheDocument();
-  });
-
-  it("should close and open on click", async () => {
-    render(
-      <Accordion>
-        <AccordionItem title="hello">test</AccordionItem>
-      </Accordion>,
-    );
-
-    expect(screen.getByText("hello")).toBeInTheDocument();
-    expect(screen.getByText("test")).toBeVisible();
-
-    await userEvent.click(screen.getByRole("button"));
-
-    // the animation should play, then it should become invisible
-    const body = screen.getByTestId("accordionBody");
-    fireEvent.animationEnd(body);
-    expect(screen.queryByText("test")).not.toBeVisible();
-
-    // now click to open
-    await userEvent.click(screen.getByRole("button"));
-    fireEvent.animationEnd(body);
-    expect(screen.queryByText("test")).toBeVisible();
-  });
-
-  it("should open and close when clicked an excessive amount of times", async () => {
-    render(
-      <Accordion>
-        <AccordionItem title="hello">test</AccordionItem>
-      </Accordion>,
-    );
-    const body = screen.getByTestId("accordionBody");
-
-    // spam click the accordion
-    for (let i = 0; i < 10; i++) {
-      await userEvent.click(screen.getByRole("button"));
-      fireEvent.animationEnd(body);
-      expect(screen.queryByText("test")).not.toBeVisible();
-
-      await userEvent.click(screen.getByRole("button"));
-      fireEvent.animationEnd(body);
-      expect(screen.queryByText("test")).toBeVisible();
-    }
   });
 });
